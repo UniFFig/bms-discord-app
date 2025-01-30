0b57eeecbc7100b78d5c2893e868a32e9dea9f64
andy <hinrichsad@gmail.com>
2025-01-30 15:54:40 -0500

    updated port

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ import interactionHandler from './interactionHandler.js';
 
 // Create an express app
 const app = express();
-// Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
 
 interactionHandler(app);
 
-console.log("TESTTESTTEST")
-
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
 });
